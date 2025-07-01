b2b71f66263d4bfa9b3a291909b085b98acb5c93
SubhodeepSamanta <subhodeepsamanta2005@gmail.com>
2025-07-01 13:40:38 +0530

    add clerk auth

--- Frontend/src/Layouts/RootLayout/RootLayout.jsx
+++ Frontend/src/Layouts/RootLayout/RootLayout.jsx
@@ -14,7 +14,7 @@ const queryClient = new QueryClient()
 
 const RootLayout = () => {
   return (
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} proxyUrl={import.meta.env.VITE_CLERK_PROXY_URL} domain={import.meta.env.VITE_DOMAIN} afterSignOutUrl="/">
       <QueryClientProvider client={queryClient}>
       <div className="rootLayout">
         <header>
