75ef3e08ee1b1982e606b66042552064018dbf78
SubhodeepSamanta <subhodeepsamanta2005@gmail.com>
2025-07-06 00:24:24 +0530

    add backend firebase

--- Backend/index.js
+++ Backend/index.js
@@ -5,16 +5,26 @@ import connectDB from "./utilities/connectDB.js";
 import Chat from "./models/chat.model.js";
 import UserChats from "./models/userChats.model.js";
 import admin from "firebase-admin";
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
-const serviceAccount = require("./firebaseServiceAccount.json");
 
-const app = express();
-const PORT = process.env.PORT;
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
+
+const app = express();
+const PORT = process.env.PORT;
 
 const firebaseAuthMiddleware = async (req, res, next) => {
   const authHeader = req.headers.authorization;
